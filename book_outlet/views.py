143a5fc1bffcf52d9884af5084690e3a4238cdac
duongbuibd <duongbuibd@gmail.com>
2024-01-13 18:02:15 +0700

    dynamic data

--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+user = {
+    "name": "Danny Duong",
+    'job': "Software Engineer",
+    'address': "Viet Nam"
+}
+
+
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, guys!")
+    return render(request, "index.html", {
+        "data": user
+    })
